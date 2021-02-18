2e37527d1f1919e39ca6de99f6ddc1beb765a27e
kilaru-hindu <79193140+kilaru-hindu@users.noreply.github.com>
2021-02-18 16:50:52 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Git hub documentation'
+copyright = '2021, hindu'
+author = 'APSSDC'
 
 # The full version, including alpha/beta/rc tags
 release = ''
